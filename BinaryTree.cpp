4990ab395791ac895e9508933b7658d81ec01872
Zalain Spain <2023159874@ub.edu.bz>
2025-03-30 04:05:03 -0600

    added functions and code for inorderTraversal function

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -15,4 +15,60 @@ BinaryTree::~BinaryTree(){
     
 }
 
+void BinaryTree::insert(int value){
+
+    if(root == nullptr){
+        root = new TreeNode(value);  //create new node if tree is empty
+        return;
+    }
+    
+}
+
+bool BinaryTree::search(int value) const{
+
+    
+
+}
+
+void BinaryTree::deleteNode(int value){
+
+    
+}
+
+void BinaryTree::inorderTraversal() const{
+
+    if(root == nullptr){
+        cout<<"The tree is empty"<<endl;
+        return;
+    }
+    inorderTraversal(root->left);
+    cout<<root->value<<endl;
+    inorderTraversal(root->right);
+
+}
+
+void BinaryTree::preorderTraversal() const{
+
+    
+}
+
+void BinaryTree::postorderTraversal() const{
+
+    
+}
+
+void BinaryTree::levelOrderTraversal() const{
+
+    
+}
+
+int BinaryTree::count() const{
+
+    
+}
+
+int BinaryTree::findHeight() const{ 
+
+    
+}
 
