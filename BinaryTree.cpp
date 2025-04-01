46f7efa20174fca37a8255810c7d6a0a8ced428c
Zalain Spain <2023159874@ub.edu.bz>
2025-04-01 01:49:53 -0600

    modified functions and debugged

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -11,6 +11,7 @@ BinaryTree::BinaryTree(){
 BinaryTree::~BinaryTree(){
     //destructor to delete all nodes in the tree
     destroyTree(root); //call the private helper function to delete all nodes in the tree
+    cout<<"Tree destroyed"<<endl; //output message
     
 }
 
@@ -21,6 +22,7 @@ void BinaryTree::destroyTree(TreeNode* node){
     destroyTree(node->left); //delete left child node
     destroyTree(node->right); //delete right child node
     delete node; //delete the current node
+    root = nullptr;
 }
 
 void BinaryTree::insert(int value){
@@ -29,31 +31,32 @@ void BinaryTree::insert(int value){
 
 void BinaryTree::insert(TreeNode*& node, int value){ //private helper function
 
-    if(root == nullptr){
-        root = new TreeNode(value);  //create new node if tree is empty
+    if(node == nullptr){
+        node = new TreeNode(value);  //create new node if tree is empty
         return;
     }
-    if(value<root->value){ //check if value is less, if so go to left subtree
-        insert(root->left,value);
-    }else if(value>root->value){ //check if value is greater, if so go to right subtree
-        insert(root->right,value);
+    if(value<node->value){ //check if value is less, if so go to left subtree
+        insert(node->left,value);
+    }else if(value>node->value){ //check if value is greater, if so go to right subtree
+        insert(node->right,value);
     }
 
 }
 
 bool BinaryTree::search(int value) const{
-search(root,value); //call the private helper function to search for the value in the tree
+return search(root,value); //call the private helper function to search for the value in the tree
 }
-bool BinaryTree::search(TreeNode* node, int value) const{ //private helper function
 
-    if(node ==nullptr || node->value ==value){ //check if the node is null or if the value is found
-        return root;
+bool BinaryTree::search(TreeNode* node, int value) const {
+    if (node == nullptr) {
+        return false;
     }
-    if(value < node->value){
-        search(node -> left ,value); //check if value is less, if so go to left subtree
+    if (node->value == value) {
+        return true;
     }
-    search(node->right,value); //check if value is greater, if so go to right subtree
+    return (value < node->value) ? search(node->left, value) : search(node->right, value);
 }
+
 
 void BinaryTree::deleteNode(int value){ //delete node function
 
@@ -65,6 +68,7 @@ void BinaryTree::deleteNode(int value){ //delete node function
 void BinaryTree::deleteNode(TreeNode*& node,  int value){
 
     if(node == nullptr){ //check if the tree is empty
+    cout<<"Value does not exist"<<endl;
         return;
     }
     if(value< node->value){ //check if value is less, if so go to left subtree
@@ -72,21 +76,30 @@ void BinaryTree::deleteNode(TreeNode*& node,  int value){
     }else if (value>node->value){ //check if value is greater, if so go to right subtree
         deleteNode(node->right,value); //call the private helper function to delete the node
     }else {
+        cout<< "Dlete node with the value: "<<node->value<<endl;
         if(node->left == nullptr){ //check if left child node is null
             TreeNode* temp = node;
             node = node->right; //set the left child node as the new root
             delete temp; //delete the old root
+            cout<<"Sucessfully deleted: "<<value<<endl;
 }else if(node->right == nullptr){ //check if right child node is null
+    cout<<"Deleted node with the value: "<<node->value<<endl;
     TreeNode* temp = node;
     node = node->left; //set the left child node as the new root
     delete temp; //delete the old root
 }else{
+    //if node has 2 children
 TreeNode* temp = findMin(node->right);
+cout << "Replacing node value " << node->value << " with " << temp->value << endl;
+
 node->value = temp->value;
 deleteNode(node->right, temp->value);
+cout<<"Deleted node with the value: "<<node->value<<endl;
 }
+cout << "Successfully deleted: " << value << endl;
+    }
 }
-}
+
 
 void BinaryTree::inorderTraversal() const{ // call the private helper function
 
@@ -94,7 +107,9 @@ void BinaryTree::inorderTraversal() const{ // call the private helper function
         cout<<"The tree is empty"<<endl; //if empty output message and return
         return;
     }
+    cout<<"in order traversal: ";
     inorderTraversal(root); //call the private helper function
+    cout<<endl;
 
 }
 void BinaryTree::inorderTraversal(TreeNode* node) const{ //private helper function
@@ -103,7 +118,7 @@ void BinaryTree::inorderTraversal(TreeNode* node) const{ //private helper functi
         return;
     }
     inorderTraversal(node->left); //call the left child node
-    cout<<node->value<<endl; //ouput value of the node
+    cout<<node->value<<" "; //ouput value of the node
     inorderTraversal(node->right); //call the right child node
     
 }
@@ -114,14 +129,16 @@ void BinaryTree::preorderTraversal() const{ //call the private helper function
         cout<<"The tree is empty"<<endl; //if empty output message and return
         return;
     }
+    cout<<"pre order traversal: ";
     preorderTraversal(root); //call the private helper function
+    cout<<endl;
     
 }
 void BinaryTree::preorderTraversal(TreeNode* node) const{ //private helper function
     if(node == nullptr){ //check if the node is null
         return;
 }
-cout<<node->value<<endl; //output the node value
+cout<<node->value<<" "; //output the node value
 preorderTraversal(node->left); //call the left child node
 preorderTraversal(node->right); // call the right child node
 }
@@ -132,7 +149,10 @@ void BinaryTree::postorderTraversal() const{ //call the private helper function
         cout<<"The tree is empty"<<endl; //if empty output message and return
         return;
     }
+    cout<<"Post order traversal: ";
     postorderTraversal(root); //call the private helper function
+    cout<<endl;
+
 }
 
 void BinaryTree::postorderTraversal(TreeNode* node) const{ //private helper function
@@ -141,7 +161,7 @@ void BinaryTree::postorderTraversal(TreeNode* node) const{ //private helper func
     }
     postorderTraversal(node->left); //call the left child node
     postorderTraversal(node->right); //call the right child node
-    cout<<node->value<<endl; //output the node value
+    cout<<node->value<<" "; //output the node value
 }
 
 void BinaryTree::levelOrderTraversal() const{ //level order traversal function
@@ -150,22 +170,17 @@ void BinaryTree::levelOrderTraversal() const{ //level order traversal function
         cout<<"The tree is empty"<<endl; //if empty output message and return
         return;
     }
+    cout<<"level order traversal: ";
     queue <TreeNode*> q; //create a queue to store the nodes
     q.push(root); //push the root node into a queue
 
     while(!q.empty()) //while the queue is empty
     {
-        int levelsize = q.size(); //get the size of the queue
-        for(int i =0; i<levelsize; i++){
 
         TreeNode* current = q.front(); //get the node in front of the queue
         q.pop(); //remove the node from the queue
 
-        cout<<current->value<<endl; //output the node value
-
-            if(i<levelsize -1){ //add comma between the values
-                cout<<", "; 
-            }
+        cout<<current->value<<" "; //output the node value
 
         if(current->left != nullptr) //check if the left child node is not null
         {
@@ -176,13 +191,12 @@ void BinaryTree::levelOrderTraversal() const{ //level order traversal function
             q.push(current->right); //push the right child node into the queue
         }
     }
-    cout<<endl; 
+    cout<<endl;
  }
-}
 
 
 int BinaryTree::count() const{
-    return count(root); //call the private helper function
+    return count(root);
         
     }
 
@@ -195,7 +209,9 @@ int BinaryTree::count(TreeNode* node) const{ //private helper function
 }
 
 int BinaryTree::findHeight() const{ 
-    return findHeight(root); //call the private helper function
+    int height = findHeight(root);
+    cout<<"height of the tree: "<<height<<endl;
+    return height; //call the private helper function
     
 }
 int BinaryTree::findHeight(TreeNode* node) const{
@@ -204,7 +220,7 @@ int BinaryTree::findHeight(TreeNode* node) const{
     }
     int left = findHeight(node->left); //height of the left subtree
     int right = findHeight(node->right); //hieght of the right subtree
-    return max(left,right); //return the max height betweeen the left and right subtrees
+    return 1 + max(left,right); //return the max height betweeen the left and right subtrees
 }
 
 
--- main.cpp
+++ main.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
+#include "BinaryTree.h"
+
 using namespace std;
 
 int main() {
     
-    
+    BinaryTree tree;
+
+tree.insert(50);
+tree.insert(30);
+tree.insert(70);
+tree.insert(20);
+tree.insert(40);
+tree.insert(60);
+tree.insert(80);
+tree.inorderTraversal();   // Output: 20 30 40 50 60 70 80
+tree.preorderTraversal();  // Output: 50 30 20 40 70 60 80
+tree.postorderTraversal(); // Output: 20 40 30 60 80 70 50
+tree.levelOrderTraversal(); // Output: 50 30 70 20 40 60 80
+tree.count();         // Output: 7
+tree.findHeight();    // Output: 2 (height of the tree)
+tree.findMin();       // Output: 20 (minimum value in the tree)
+tree.search(40);  // Output: Found
+tree.search(90);  // Output: Not Found
+tree.deleteNode(30);
+tree.inorderTraversal();   // Output should reflect deletion
+
 
 }
